d0b1ca63c54d6d37b2d40b1e27369d979832bd8e
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2023-03-19 17:37:27 -0400

    added javascript to load questions, and correct anwsers, and timer

---
+++ assets/script.js
@@ -0,0 +1,102 @@
+var startButton = document.querySelector(".start-button");
+var questionsEL = document.querySelector(".questions");
+var answersEL = document.querySelector(".answers");
+var currentQuestion = 0;
+var correctAnswers = 0;
+var submitButton;
+var allQuestions = {
+    'Commonly used data types DO NOT include: ': ['1: Strings', '2: Booleans', '3: Alerts', '4: Numbers', 2],
+    'The condition in an if/else statement is enclosed with ______: ': ['1: Quotes', '2: Curly Brackets', '3: Parenthesis', '4: Square Brackets', 2],
+    'Arrays in JavaScript can be used to store ______:': ['1: Numbers and Strings', '2: Other arrays', '3: Booleans', '4: all of the above', 3],
+    'String values must be inclosed within _______ when being assigned to variables: ': ['1: Commas', '2: Curly Brackets', '3: Quotes', '4: Parenthesis', 0],
+    'A very useful tool used during development and debugging for printing content to the debugger is: ': ['1: JavaScript', '2: Terminal/Bash', '3: For Loops', '4: Console.log', 3],
+};
+
+function loadQuestion(currentQuestion) {
+    var question = Object.keys(allQuestions)[currentQuestion];
+    questionsEL.innerHTML = '';
+    questionsEL.innerHTML = question;
+};
+
+function loadAnswers(currentQuestion) {
+    var answers = allQuestions[Object.keys(allQuestions)[currentQuestion]];
+    answersEL.innerHTML = '';
+    for (var i = 0; i < answers.length - 1; i += 1) {
+        var createButton = document.createElement('button'),
+            text = document.createTextNode(answers[i]);
+        createButton.appendChild(text);
+        createButton.addEventListener("click", checkAnswer(i, answers));
+        answersEL.appendChild(createButton);
+    }
+};
+
+function checkAnswer(i, arr) {
+    return function () {
+        var userAnswer = i,
+            correctAnswer = arr[arr.length - 1];
+        if (userAnswer === correctAnswer) {
+            correctAnswers += 1;
+        } else {
+            timerCount = timerCount - 10;
+        }
+        if (question < Object.keys(allQuestions).length - 1) {
+            question += 1;
+            loadQuestion(question);
+            loadAnswers(question);
+        } else {
+            answersEL.innerHTML = '';
+            allDone();
+        }
+    };
+};
+
+var timerEl = document.querySelector(".timer");
+function submitScore() {
+    location.reload();
+}
+function allDone() {
+    var inputEL = document.createElement("input");
+    var inputElLabel = document.createElement("label");
+    submitButton = document.createElement("button");
+    inputElLabel.setAttribute("for", inputEL);
+    submitButton.textContent = "Submit";
+    inputElLabel.textContent = " Enter your initials: ";
+    inputEL.type = "text";
+    timerCount = 0;
+    timerEl.textContent = timerCount;
+    clearInterval(timer);
+    questionsEL.textContent = "GAME OVER!";
+    answersEL.textContent = "Your final score is: " + correctAnswers;
+    answersEL.appendChild(inputElLabel);
+    answersEL.appendChild(inputEL);
+    answersEL.appendChild(submitButton);
+    submitButton.addEventListener("click", submitScore);
+}
+
+var timer;
+var timerCount;
+function startTimer() {
+    timerCount = 60;
+    timerEl.textContent = timerCount;
+    timer = setInterval(function () {
+        timerCount--;
+        timerEl.textContent = timerCount;
+        if (timerCount === 0) {
+            clearInterval(timer);
+            startButton.disabled = true;
+        }
+    }, 1000);
+};
+
+function startTest() {
+    question = 0;
+    document.getElementById("start-button").style.visibility = "hidden";
+    startTimer();
+    loadQuestion(question);
+    loadAnswers(question);
+};
+
+function init() {
+    startButton.addEventListener("click", startTest);
+};
+init();
